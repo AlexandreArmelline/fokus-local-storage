edc5638a16546260d75dedc058005171bcf4e4f0
ALEXANDRE ARMELLINE DA SILVA <alexandrearmelline@gmail.com>
2023-11-22 20:57:31 -0300

    Update script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -1,7 +1,18 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionartarefa = document.querySelector('.app__form-add-task')
+const textarea = document.querySelector('.app__form-textarea')
 
+const tarefas = []
 
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionartarefa.classList.toggle('hidden')
+})
+
+formAdicionartarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textarea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', tarefas)
 })
