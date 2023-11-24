11c313506cde999ed5c8a031418f2bc687c85746
ALEXANDRE ARMELLINE DA SILVA <alexandrearmelline@gmail.com>
2023-11-24 17:28:35 -0300

    Update script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -4,6 +4,32 @@ const textarea = document.querySelector('.app__form-textarea')
 
 const tarefas = []
 
+function criarElementoTarefa(tarefa) {
+    const li = document.createElement('li')
+    li.classList.add('app_section-task-list-item')
+
+    const svg = document.createElement('svg')
+    svg.innerHTML= `
+    <svg>
+        <svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    </svg>
+    `
+    const paragrafo = document.createElement('p')
+    paragrafo.textContent = tarefa.descricao
+
+    const botao = document.createElement('button')
+    const imagemBotao = document.createElement('img')
+    imagemBotao.setAttribute('src', '/imagens/edit.png')
+    botao.append(imagemBotao)
+
+    li.append(svg)
+    li.append(paragrafo)
+    li.append(botao)
+}
+
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionartarefa.classList.toggle('hidden')
 })
@@ -16,3 +42,20 @@ formAdicionartarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa)
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
 })
+
+
+/* <li class="app_section-task-list-item">
+    <svg>
+        <svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    </svg>
+    <p class="app_section-task-list-item-description">
+        Estudando localStorage
+    </p>
+    <button class="app_button-edit">
+        <img src="/imagens/edit.png">
+    </button>
+</li>*/
+
