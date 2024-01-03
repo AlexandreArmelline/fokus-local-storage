39a6af4e17a7b1e10c8d32a672ef6278b20966b1
ALEXANDRE ARMELLINE DA SILVA <alexandrearmelline@gmail.com>
2024-01-03 16:44:35 -0300

    Update script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -2,6 +2,8 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionartarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
@@ -28,9 +30,9 @@ function criarElementoTarefa(tarefa) {
     botao.classList.add('app_button-edit')
 
     botao.onclick = () => {
-        
+        //debugger
         const novaDescricao = prompt("Qual é o novo nome da tarefa?")
-        console.log('Nova descrição da tarefa: ', novaDescricao)
+        //console.log('Nova descrição da tarefa: ', novaDescricao)
         if (novaDescricao) {
             paragrafo.textContent = novaDescricao
             tarefa.descricao = novaDescricao
@@ -45,6 +47,12 @@ function criarElementoTarefa(tarefa) {
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
+
+    li.onclick = () => {
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+        li.classList.add('app__section-task-list-item-active')
+
+    }
 
     return li
 }
@@ -69,6 +77,11 @@ formAdicionartarefa.addEventListener('submit', (evento) => {
 tarefas.forEach(tarefa => {
    const elementoTarefa = criarElementoTarefa(tarefa)
    ulTarefas.append(elementoTarefa)
+})
+
+btnCancelar.addEventListener('click', () => {
+    formAdicionartarefa.classList.toggle('hidden')
+    textarea.value = '';
 })
 
 
